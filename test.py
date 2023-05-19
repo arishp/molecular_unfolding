86877c6e94b25c7ff7ac50e91260226866d989ae
Arish P <arish.p@gmail.com>
2023-05-19 20:49:11 +0000

    update test.py

--- test.py
+++ test.py
@@ -1,92 +1,34 @@
-from sympy import *
-import numpy as np
+import dimod
+import neal
+from dwave.system import DWaveSampler, EmbeddingComposite
 
-
-d = 4
-angle = 0.0
-angle_incr = 2*np.pi/d
-d_angles = [angle]
-for i in range(1, d):
-    angle += angle_incr
-    d_angles.append(angle)
-print(d_angles)
-
-
-
-x11, x12, x13, x14 = symbols('x11, x12, x13, x14')
-init_printing(use_unicode=False, wrap_line=False)
-e = (x11 + x12 + x13 + x14 - 1)*(x11 + x12 + x13 + x14 - 1)
-print(e.as_poly())
 
 hubo = {}
-
-A_const= 1.0
 ### Hard constraint
-hubo[('x11', 'x11')] = 1 * A_const
-hubo[('x11', 'x12')] = 2.0 * A_const
-hubo[('x11', 'x13')] = 2.0 * A_const
-hubo[('x11', 'x14')] = 2.0 * A_const
-hubo[('x11', )] = -2.0 * A_const
-hubo[('x12', 'x12')] = 1 * A_const
-hubo[('x12', 'x13')] = 2.0 * A_const
-hubo[('x12', 'x14')] = 2.0 * A_const
-hubo[('x12', )] = -2.0 * A_const
-hubo[('x13', 'x13')] = 1 * A_const
-hubo[('x13', 'x14')] = 2.0 * A_const
-hubo[('x13', )] = -2.0 * A_const
-hubo[('x14', 'x14')] = 1 * A_const
-hubo[('x14', )] = -2.0 * A_const
-hubo[()] = 1.0 * A_const
-print(hubo)
+hubo[('x00', 'x00')] = 18.0
+hubo[('x00', 'x01')] = 40.0
+hubo[('x00', 'x02')] = 44.0
+hubo[('x00', 'x03')] = 40.0
+hubo[('x00', )] = -38.0
+hubo[('x01', 'x01')] = 18.0
+hubo[('x01', 'x02')] = 40.0
+hubo[('x01', 'x03')] = 44.0
+hubo[('x01', )] = -40.0
+hubo[('x02', 'x02')] = 18.0
+hubo[('x02', 'x03')] = 40.0
+hubo[('x02', )] = -42.0
+hubo[('x03', 'x03')] = 18.0
+hubo[('x03', )] = -40.0
+hubo[()] =  12.75
+print("\nHUBO:\n", hubo)
 
-### optimizing constraint
-#C1, C4 - X
-hubo[('x11', 'x11')] -= 1
-hubo[('x11', 'x13')] -= 2.0
-hubo[('x11', )] -= -2.0
-hubo[('x13', 'x13')] -= 1
-hubo[('x13', )] -= 2.0
-hubo[()] -= 1.0
-#C1, C4 - Y
-hubo[('x11', 'x11')] -= 2.25
-hubo[('x11', 'x12')] -= 4.5
-hubo[('x11', 'x13')] -= 4.5
-hubo[('x11', 'x14')] -= 4.5
-hubo[('x11', )] -= -1.5
-hubo[('x12', 'x12')] -= 2.25
-hubo[('x12', 'x13')] -= 4.5
-hubo[('x12', 'x14')] -= 4.5
-hubo[('x12', )] -= 1.5
-hubo[('x13', 'x13')] -= 2.25
-hubo[('x13', 'x14')] -= 4.5
-hubo[('x13', )] -= 1.5
-hubo[('x14', 'x14')] -= 2.25
-hubo[('x14', )] -= 1.5
-hubo[()] -= 0.25
-#C1, C4 - Z
-hubo[('x12', 'x12')] -= 1
-hubo[('x12', 'x14')] -= -2.0
-hubo[('x14', 'x14')] -= 1
+bqm = dimod.make_quadratic(hubo, 12.0, dimod.BINARY)
 
+sampler = neal.SimulatedAnnealingSampler()
+sample_size=10
+sampleset = sampler.sample(bqm, num_reads=sample_size)
+print("\nSA RESULTS:\n",sampleset)
 
-#C2, C4 - X
-hubo[('x11', 'x11')] -= 1
-hubo[('x11', 'x13')] -= -2.0
-hubo[('x13', 'x13')] -= 1
-#C2, C4 - Y
-hubo[('x11', 'x11')] -= 2.25
-hubo[('x11', 'x12')] -= 4.5
-hubo[('x11', 'x13')] -= 4.5
-hubo[('x11', 'x14')] -= 4.5
-hubo[('x12', 'x12')] -= 2.25
-hubo[('x12', 'x13')] -= 4.5
-hubo[('x12', 'x14')] -= 4.5
-hubo[('x13', 'x13')] -= 2.25
-hubo[('x13', 'x14')] -= 4.5
-hubo[('x14', 'x14')] -= 2.25
-#C2, C4 - Z
-hubo[('x12', 'x12')] -= 1
-hubo[('x12', 'x14')] -= -2.0
-hubo[('x14', 'x14')] -= 1
-
-#print(hubo)
+sampler = EmbeddingComposite(DWaveSampler())
+sampleset = sampler.sample(bqm, num_reads=1000)
+print("\nQA RESULTS:\n",sampleset)
